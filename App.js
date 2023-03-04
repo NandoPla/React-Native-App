7aeae76ae38e533f95f660bdb3eea31a4033400d
NandoPla <fernandopla8@gmail.com>
2023-03-04 01:45:07 -0300

    fix: colors

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Esse é o título principal</Text>
+      <StatusBar style="light" />
+      <TextInput></TextInput>
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: "#fff"
+  }
 });
